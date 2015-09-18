864f9d252b4cd31cf910d7ae1454433d0f5b3fcd
Desno365 <desno365@gmail.com>
2015-09-18 23:09:25 +0200

    Remove Information UI; Update IDs; Fix discs removed in creative; Other improvements

--- Jukebox_Mod_Desno365.js
+++ Jukebox_Mod_Desno365.js
@@ -14,54 +14,67 @@ SOFTWARE.
 
 /* ******* Jukebox Mod by Desno365 ******* */
 
+// minecraft variables
+const GameMode = {
+	SURVIVAL: 0,
+	CREATIVE: 1
+};
+
+//activity and other Android variables
+var currentActivity = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+var sdcard = android.os.Environment.getExternalStorageDirectory();
+
+//display size and density variables
+var metrics = new android.util.DisplayMetrics();
+currentActivity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+var deviceDensity = metrics.density;
+metrics = null;
+
 //number of jukeboxes variable
 var nOfJ = 0;
 
 //"now playing" message variables
-var nowPlayingMessageBool = false;
-var countdownForNowPlayingMessage = 0;
-var timesChanged = 0;
-var currentColor = 0;
 var nowPlayingMessage = "";
+var currentColor = 0;
 
 //other variables
 const MAX_LOGARITHMIC_VOLUME = 65;
-const JUKEBOX_SONGS_PATH = (new android.os.Environment.getExternalStorageDirectory() + "/games/com.mojang/minecraft-jukebox/");
-var initCreativeItems = true;
+const JUKEBOX_SONGS_PATH = sdcard + "/games/com.mojang/minecraft-jukebox/";
 var jukeboxes = [];
-var discNames = ["11 Disc", "13 Disc", "Blocks Disc", "Cat Disc", "Chirp Disc", "Far Disc", "Mall Disc", "Mellohi Disc", "Stal Disc", "Strad Disc", "Wait Disc", "Ward Disc"];
-var discSongs = ["11.ogg", "13.ogg", "blocks.ogg", "cat.ogg", "chirp.ogg", "far.ogg", "mall.ogg", "mellohi.ogg", "stal.ogg", "strad.ogg", "wait.ogg", "ward.ogg"];
-var currentActivity = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+var discNames = ["13 Disc", "Cat Disc", "Blocks Disc", "Chirp Disc", "Far Disc", "Mall Disc", "Mellohi Disc", "Stal Disc", "Strad Disc", "Ward Disc", "11 Disc", "Wait Disc"];
+var discSongs = ["13.ogg", "cat.ogg", "blocks.ogg", "chirp.ogg", "far.ogg", "mall.ogg", "mellohi.ogg", "stal.ogg", "strad.ogg", "ward.ogg", "11.ogg", "wait.ogg"];
 
 
-ModPE.setItem(415, "record_11", 0, "11 Disc", 1);
+ModPE.setItem(2256, "record_13", 0, "13 Disc", 1);
 
-ModPE.setItem(416, "record_13", 0, "13 Disc", 1);
+ModPE.setItem(2257, "record_cat", 0, "Cat Disc", 1);
 
-ModPE.setItem(417, "record_blocks", 0, "Blocks Disc", 1);
+ModPE.setItem(2258, "record_blocks", 0, "Blocks Disc", 1);
 
-ModPE.setItem(418, "record_cat", 0, "Cat Disc", 1);
+ModPE.setItem(2259, "record_chirp", 0, "Chirp Disc", 1);
 
-ModPE.setItem(419, "record_chirp", 0, "Chirp Disc", 1);
+ModPE.setItem(2260, "record_far", 0, "Far Disc", 1);
 
-ModPE.setItem(420, "record_far", 0, "Far Disc", 1);
+ModPE.setItem(2261, "record_mall", 0, "Mall Disc", 1);
 
-ModPE.setItem(421, "record_mall", 0, "Mall Disc", 1);
+ModPE.setItem(2262, "record_mellohi", 0, "Mellohi Disc", 1);
 
-ModPE.setItem(422, "record_mellohi", 0, "Mellohi Disc", 1);
+ModPE.setItem(2263, "record_stal", 0, "Stal Disc", 1);
 
-ModPE.setItem(423, "record_stal", 0, "Stal Disc", 1);
+ModPE.setItem(2264, "record_strad", 0, "Strad Disc", 1);
 
-ModPE.setItem(424, "record_strad", 0, "Strad Disc", 1);
+ModPE.setItem(2265, "record_ward", 0, "Ward Disc", 1);
 
-ModPE.setItem(425, "record_wait", 0, "Wait Disc", 1);
+ModPE.setItem(2266, "record_11", 0, "11 Disc", 1);
 
-ModPE.setItem(426, "record_ward", 0, "Ward Disc", 1);
+ModPE.setItem(2267, "record_wait", 0, "Wait Disc", 1);
 
-Block.defineBlock(84, "Jukebox", [["jukebox_side", 0], ["jukebox_top", 0], ["jukebox_side", 0], ["jukebox_side", 0], ["jukebox_side", 0], ["jukebox_side", 0]]);
-Block.setDestroyTime(84, 2);
-Block.setExplosionResistance(84, 30);
-Item.addShapedRecipe(84, 1, 0, [
+
+const JUKEBOX_ID = 84;
+Block.defineBlock(JUKEBOX_ID, "Jukebox", [["jukebox_side", 0], ["jukebox_top", 0], ["jukebox_side", 0], ["jukebox_side", 0], ["jukebox_side", 0], ["jukebox_side", 0]], 17);
+Block.setDestroyTime(JUKEBOX_ID, 2);
+Block.setExplosionResistance(JUKEBOX_ID, 30);
+Item.addShapedRecipe(JUKEBOX_ID, 1, 0, [
 	"www",
 	"wdw",
 	"www"], ["w", 5, 0, "d", 264, 0]);
@@ -90,7 +103,7 @@ function selectLevelHook()
 				}
 				
 				if(areSoundsMissing)
-					soundsMissingGUI(missingSoundsText.substring(0, missingSoundsText.length - 2));
+					missingSoundsUI(missingSoundsText.substring(0, missingSoundsText.length - 2));
 			}catch(err)
 			{
 				clientMessage("Error: " + err);
@@ -101,12 +114,13 @@ function selectLevelHook()
 
 function newLevel()
 {
-	if(initCreativeItems)
+	if(Level.getGameMode() == GameMode.CREATIVE)
 	{
-		Player.addItemCreativeInv(84, 1);
-		for(var i = 415; i <= 426; i++)
+		// crashes in survival
+		Player.addItemCreativeInv(JUKEBOX_ID, 1);
+
+		for(var i = 2256; i <= 2267; i++)
 			Player.addItemCreativeInv(i, 1);
-		initCreativeItems = false;
 	}
 }
 
@@ -118,24 +132,24 @@ function leaveGame()
 	nOfJ = 0;
 	jukeboxes = [];
 
-	nowPlayingMessageBool = false;
-	countdownForNowPlayingMessage = 0;
-	timesChanged = 0;
-	currentColor = 0;
 	nowPlayingMessage = "";
+	currentColor = 0;
 }
 
 function modTick()
 {
+	// jukebox sound
 	for(var i in jukeboxes)
 	{
 		jukeboxes[i].countdown++;
-		if(jukeboxes[i].countdown == 10)
+		if(jukeboxes[i].countdown >= 10)
 		{
 			jukeboxes[i].countdown = 0;
 			var distancePJ = Math.sqrt( (Math.pow(jukeboxes[i].x - Player.getX(), 2)) + (Math.pow(jukeboxes[i].y - Player.getY(), 2)) + (Math.pow(jukeboxes[i].z - Player.getZ(), 2) ));
 			if(distancePJ > MAX_LOGARITHMIC_VOLUME)
+			{
 				jukeboxes[i].player.setVolume(0.0, 0.0);
+			}
 			else
 			{
 				var volume = 1 - (Math.log(distancePJ) / Math.log(MAX_LOGARITHMIC_VOLUME));
@@ -143,38 +157,12 @@ function modTick()
 			}
 		}
 	}
-
-	if(nowPlayingMessageBool)
-	{
-		if((countdownForNowPlayingMessage % 5) == 0)
-		{
-			if(timesChanged == 1)
-			{
-				ModPE.showTipMessage(" ");
-				countdownForNowPlayingMessage = 0;
-				timesChanged = 0;
-				currentColor = 0;
-				nowPlayingMessageBool = false;
-			}else
-			{
-				ModPE.showTipMessage("§" + currentColor.toString(16) + nowPlayingMessage);
-				if(currentColor  == 15)
-				{
-					timesChanged++;
-					currentColor = 0;
-				}
-				else
-					currentColor++;
-			}
-		}
-		countdownForNowPlayingMessage++;
-	}
 }
 
 function useItem(x, y, z, itemId, blockId, side, itemDamage)
 {
 	//is block a jukebox?
-	if(Level.getTile(x, y, z) == 84)
+	if(blockId == JUKEBOX_ID)
 	{
 		preventDefault();
 
@@ -182,30 +170,26 @@ function useItem(x, y, z, itemId, blockId, side, itemDamage)
 		var checkBlockJukebox = getJukeboxObjectFromXYZ(x, y, z);
 		if(checkBlockJukebox != -1)
 		{
-			checkBlockJukebox.removePlayingJukebox();
+			checkBlockJukebox.stopJukebox();
 			return;
 		}
 
 		//is the player carrying a disc?
-		var carried = Player.getCarriedItem();
-		if(carried >= 415 && carried <= 426)
+		if(itemId >= 2256 && itemId <= 2267)
 		{
 			//jukebox: start playing
 			try
 			{
-				jukeboxes[nOfJ] = new jukebox(Math.floor(x) + 0.5, Math.floor(y), Math.floor(z) + 0.5, carried);
+				jukeboxes[nOfJ] = new JukeboxClass(Math.floor(x) + 0.5, Math.floor(y), Math.floor(z) + 0.5, itemId);
 				nOfJ++;
-				if(Player.getCarriedItemCount() == 1)
-					Player.clearInventorySlot(Player.getSelectedSlotId());
-				else
-					Entity.setCarriedItem(Player.getEntity(), carried, Player.getCarriedItemCount() - 1, 0);
+				if(Level.getGameMode() == GameMode.SURVIVAL)
+					Player.decreaseByOneCarriedItem();
 			}
 			catch(err)
 			{
 				ModPE.showTipMessage("Jukebox: Sounds not installed!");
 			}
-		}else
-			informationsForJukeboxGUI();
+		}
 	}
 }
 
@@ -216,7 +200,7 @@ function deathHook(murderer, victim)
 		var random = Math.floor((Math.random() * 30) + 365);
 		if(random == 365)
 		{
-			var randomDisc = Math.floor((Math.random() * 12) + 415);
+			var randomDisc = Math.floor((Math.random() * 12) + 2256);
 			Level.dropItem(Entity.getX(victim), Entity.getY(victim), Entity.getZ(victim), 0, randomDisc, 1, 0);
 		}
 	}
@@ -226,40 +210,72 @@ function destroyBlock(x, y, z)
 {
 	var checkBlockJukebox = getJukeboxObjectFromXYZ(x, y, z);
 	if(checkBlockJukebox != -1)
-		checkBlockJukebox.removePlayingJukebox();
+		checkBlockJukebox.stopJukebox();
 }
 
 //############################################################################
 // Added functions (No GUI and No render)
 //############################################################################
 
-function jukebox(x, y, z, disc)
+function JukeboxClass(x, y, z, disc)
 {
 	this.x = x;
 	this.y = y;
 	this.z = z;
 	this.countdown = 0;
-	this.player = new android.media.MediaPlayer();
 	this.disc = disc;
+
+	this.player = new android.media.MediaPlayer();
 	this.player.reset();
-	this.player.setDataSource(JUKEBOX_SONGS_PATH + getDataSourceFromDisc(disc));
+	this.player.setDataSource(JUKEBOX_SONGS_PATH + getFileNameFromDiscId(disc));
 	this.player.prepare();
 	this.player.setVolume(1.0, 1.0);
 	this.player.setOnCompletionListener(new android.media.MediaPlayer.OnCompletionListener()
+	{
+		onCompletion: function()
 		{
-			onCompletion: function()
-			{
-				getJukeboxObjectFromXYZ(x, y, z).removePlayingJukebox();
-			}
-		});
+			getJukeboxObjectFromXYZ(x, y, z).stopJukebox();
+		}
+	});
 	this.player.start();
 
-	nowPlayingMessageBool = true;
-	nowPlayingMessage = "Now playing: " + Item.getDiscName(disc);
-	countdownForNowPlayingMessage = 0;
+	nowPlayingMessage = "Now playing: C418 - " + getDiscName(disc);
+	currentActivity.runOnUiThread(new java.lang.Runnable(
+	{
+		run: function()
+		{
+			for(var ms = 0; ms < 17; ms++) // executed 17 times,
+			{
+				if(ms < 16)
+				{
+					new android.os.Handler().postDelayed(new java.lang.Runnable(
+					{
+						run: function()
+						{
+							ModPE.showTipMessage("§" + currentColor.toString(16) + nowPlayingMessage);
+							if(currentColor == 15)
+								currentColor = 0;
+							else
+								currentColor++;
+						}
+					}), ms * 250 + 1);
+				} else
+				{
+					new android.os.Handler().postDelayed(new java.lang.Runnable(
+					{
+						run: function()
+						{
+							ModPE.showTipMessage(" ");
+							currentColor = 0;
+						}
+					}), ms * 250 + 1);
+				}
+			}
+		}
+	}));
 
 
-	this.removePlayingJukebox = function()
+	this.stopJukebox = function()
 	{
 		this.ejectDisc();
 		this.player.reset();
@@ -281,25 +297,39 @@ function getJukeboxObjectFromXYZ(x, y, z)
 	return -1;
 }
 
-function getDataSourceFromDisc(disc)
+function getFileNameFromDiscId(discId)
 {
 	for(var i in discSongs)
-		if(disc - 415 == i)
+		if(discId - 2256 == i)
 			return discSongs[i];
 }
 
-Item.getDiscName = function(disc)
+function getDiscName(disc)
 {
 	for(var i in discNames)
-		if(disc - 415 == i)
+		if(disc - 2256 == i)
 			return discNames[i];
+}
+
+Player.decreaseByOneCarriedItem = function()
+{
+	if(Player.getCarriedItemCount() == 1)
+		Player.clearInventorySlot(Player.getSelectedSlotId());
+	else
+		Entity.setCarriedItem(Player.getEntity(), Player.getCarriedItem(), Player.getCarriedItemCount() - 1, 0);
+}
+
+function convertDpToPixel(dp)
+{
+	//
+	return Math.round(dp * deviceDensity);
 }
 
 //############################################################################
 // GUI functions
 //############################################################################
 
-function informationsForJukeboxGUI()
+function missingSoundsUI(soundsText)
 {
 	currentActivity.runOnUiThread(new java.lang.Runnable()
 	{
@@ -307,93 +337,41 @@ function informationsForJukeboxGUI()
 		{
 			try
 			{
-				var layoutInfo = new android.widget.LinearLayout(currentActivity);
-				layoutInfo.setOrientation(android.widget.LinearLayout.VERTICAL);
+				var layout = new android.widget.LinearLayout(currentActivity);
+				var padding = convertDpToPixel(8);
+				layout.setPadding(padding, padding, padding, padding);
+				layout.setOrientation(android.widget.LinearLayout.VERTICAL);
 
-				var scrollInfo = new android.widget.ScrollView(currentActivity);
-				scrollInfo.addView(layoutInfo);
+				var scroll = new android.widget.ScrollView(currentActivity);
+				scroll.addView(layout);
 			
-				var popupInfo = new android.app.Dialog(currentActivity); 
-				popupInfo.setContentView(scrollInfo);
-				popupInfo.setTitle(new android.text.Html.fromHtml("Jukebox Mod by Desno365"));
-
-				var infoText = new android.widget.TextView(currentActivity);
-				infoText.setText(new android.text.Html.fromHtml("Welcome to the Jukebox Mod by Desno365! This mod is a porting of the Jukebox that you can find in Minecraft PC.<br><br>" +
-					"<b>Jukebox informations</b>:<br>" +
-					"-Tap the jukebox with a disc and it will start playing the choosed song.<br>" +
-					"-Greater the distance between you and the Jukebox lower the volume of it.<br>" +
-					"-<i>Jukebox ID</i>: 84.<br><br>" +
-					"<b>Disc informations</b>:<br>" +
-					"-There are twelve different discs with twelve different songs (songs ported from Minecraft PC).<br>" +
-					"-There is a random chance of finding a disc every 30 kills of hostile mobs.<br>" +
-					"-<i>Discs ID</i>: 415≈426.<br>"));
-				layoutInfo.addView(infoText);
-
-				var exitInfoButton = new android.widget.Button(currentActivity); 
-				exitInfoButton.setText("Close"); 
-				exitInfoButton.setOnClickListener(new android.view.View.OnClickListener()
-				{ 
-					onClick: function()
-					{ 
-						popupInfo.dismiss();
-					}
-				}); 
-				layoutInfo.addView(exitInfoButton); 
-				
-
-				popupInfo.show();
-			
-			}catch(err)
-			{
-				clientMessage("Error: " + err);
-				clientMessage("Maybe GUI is not supported for your device. Report this error in the official minecraftforum.net thread, please.");
-			}
-		}
-	});
-}
-
-function soundsMissingGUI(soundsText)
-{
-	currentActivity.runOnUiThread(new java.lang.Runnable()
-	{
-		run: function()
-		{
-			try
-			{
-				var layoutMissing = new android.widget.LinearLayout(currentActivity);
-				layoutMissing.setOrientation(android.widget.LinearLayout.VERTICAL);
-
-				var scrollMissing = new android.widget.ScrollView(currentActivity);
-				scrollMissing.addView(layoutMissing);
-			
-				var popupMissing = new android.app.Dialog(currentActivity); 
-				popupMissing.setContentView(scrollMissing);
-				popupMissing.setTitle(new android.text.Html.fromHtml("Jukebox Mod: ERROR"));
+				var popup = new android.app.Dialog(currentActivity); 
+				popup.setContentView(scroll);
+				popup.setTitle(new android.text.Html.fromHtml("Jukebox Mod: ERROR"));
 				
 				var missingText = new android.widget.TextView(currentActivity);
-				missingText.setText(new android.text.Html.fromHtml("<b>ERROR</b>: missing sounds.<br> " +
+				missingText.setText(new android.text.Html.fromHtml("<b>Jukebox Mod by Desno365 ERROR</b>: missing sounds.<br> " +
 				"These sounds are missing: " + soundsText + ".<br><br>" +
 				'<b><i>IMPORTANT</b></i>: you have to place the "minecraft-jukebox" folder (the folder is inside the zip that contains the mod) in "sdcard/games/com.mojang/".'));
-				layoutMissing.addView(missingText);
+				layout.addView(missingText);
 				
-				var exitMissingButton = new android.widget.Button(currentActivity); 
-				exitMissingButton.setText("Close"); 
-				exitMissingButton.setOnClickListener(new android.view.View.OnClickListener()
+				var exitButton = new android.widget.Button(currentActivity); 
+				exitButton.setText("Close"); 
+				exitButton.setOnClickListener(new android.view.View.OnClickListener()
 				{
 					onClick: function()
 					{
-						popupMissing.dismiss();
+						popup.dismiss();
 					}
 				}); 
-				layoutMissing.addView(exitMissingButton); 
+				layout.addView(exitButton); 
 				
 
-				popupMissing.show();
+				popup.show();
 			
 			}catch(err)
 			{
 				clientMessage("Error: " + err);
-				clientMessage("Maybe GUI is not supported for your device. Report this error in the official minecraftforum.net thread, please.");
 			}
 		}
 	});
